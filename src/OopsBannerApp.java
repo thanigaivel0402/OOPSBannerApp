a14094b7249458b0f4f53e024bafbeb83b17a237
thanigaivel0402 <thanigaivel.dev@bridgelabz.com>
2026-01-29 14:37:32 +0530

    [Thanigaivel] Added: UC2: Render OOPS as Banner using Print Statements.

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,5 +1,38 @@
 public class OopsBannerApp {
     public static void main(String[] args) {
+        // Line 1
+        System.out.println(
+                "  *****  " + "  *****  " + " ******  " + "  *****  "
+        );
 
+        // Line 2
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 3
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 4
+        System.out.println(
+                " *     * " + " *     * " + " ******  " + "  *****  "
+        );
+
+        // Line 5
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 6
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 7
+        System.out.println(
+                "  *****  " + "  *****  " + " *        " + "  *****  "
+        );
     }
 }
