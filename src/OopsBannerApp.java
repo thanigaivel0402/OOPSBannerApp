3b40acd6eedeee9010e46611c1765cd73e4952e8
thanigaivel0402 <thanigaivel.dev@bridgelabz.com>
2026-01-29 14:51:54 +0530

    [Thanigaivel] Added: UC6: Refactor Banner Logic into Functions.

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,5 +1,70 @@
+/**
+ * OOPSBannerApp UC6 – Render OOPS as Banner using Modular Methods
+ *
+ * This use case refactors the banner logic into helper methods
+ * to improve reusability, readability, and maintainability.
+ *
+ * @author Developer
+ * @version 6.0
+ */
 public class OopsBannerApp {
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[] {
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[] {
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *        ",
+                " *        ",
+                " *        "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[] {
+                "  *****  ",
+                " *        ",
+                " *        ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                "  *****  "
+        };
+    }
+
+    // Main method to display the OOPS banner
     public static void main(String[] args) {
 
+        // Retrieve patterns
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Assemble and print banner line by line
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    String.join("",
+                            oPattern[i],
+                            oPattern[i],   // 'O' repeated twice
+                            pPattern[i],
+                            sPattern[i]
+                    )
+            );
+        }
     }
 }
